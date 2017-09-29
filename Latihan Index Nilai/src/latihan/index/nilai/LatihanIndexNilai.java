07944c42b7baca7bb106cf54af0790c29c0eef5c
fataelislami <santribloggers@gmail.com>
2017-09-29 17:35:07 +0700

    change call variable with getter

--- Latihan Index Nilai/src/latihan/index/nilai/LatihanIndexNilai.java
+++ Latihan Index Nilai/src/latihan/index/nilai/LatihanIndexNilai.java
@@ -17,26 +17,27 @@ public class LatihanIndexNilai {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        Mahasiswa oMhs =new Mahasiswa("Fata El Islami","10116499",75,45,34);
-        double na=oMhs.getNilaiAkhir(oMhs.quiz, oMhs.uts,oMhs.uas);
+         Mahasiswa oMhs =new Mahasiswa("Fata El Islami","10116499",75,45,34);
+        double na=oMhs.getNilaiAkhir(oMhs.getQuiz(), oMhs.getUts(),oMhs.getUas());
     
         
         for(int i=1;i<=4;i++){
            
-            System.out.println("Nama ke "+i+" = "+oMhs.nama);
+            System.out.println("Nama ke "+i+" = "+oMhs.getNama());
         }
         for(int j=8;j>=1;j--){
      
-            System.out.println("Nim ke "+j+" = "+oMhs.nim);
+            System.out.println("Nim ke "+j+" = "+oMhs.getNim());
         }
-        System.out.println("Quiz : "+oMhs.quiz);
-        System.out.println("Uts : "+oMhs.uts);
-        System.out.println("Uas : "+oMhs.uas);
+        System.out.println("Quiz : "+oMhs.getQuiz());
+        System.out.println("Uts : "+oMhs.getUts());
+        System.out.println("Uas : "+oMhs.getUas());
         System.out.println("\n");
         System.out.println("Nilai Akhir = "+na);
         System.out.println("\n");
         oMhs.getIndexNilai(na);
         
+       
     }
     
 }
